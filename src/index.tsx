8a9d4c586d4c78f0830124c9960dcc320074cafe
Luis Gabriel Ramos Robles <luisgabrielramosrobles@gmail.com>
2022-01-23 19:07:28 -0400

    Comments removed from public HTML, GlobalStyles created

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import App from "./App";
+import { mediaSizes } from "./assets/CSSMediaSizes";
+
+const { tablet } = mediaSizes;
+
+const GlobalStyles = createGlobalStyle`
+	body {
+		margin: 0;
+		font-family: 'Poppins', 'Montserrat', sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		@media screen and (max-width: ${tablet}) {
+			::-webkit-scrollbar {
+				display: none;
+			}
+		}
+	}
+
+	code {
+		font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+		monospace;
+	}
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin="annonymous"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <GlobalStyles />
     <BrowserRouter>
       <App />
     </BrowserRouter>

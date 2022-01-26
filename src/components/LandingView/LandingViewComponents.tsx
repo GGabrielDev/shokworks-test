51a83cd0063793349fafe90fd111b3abcc5be7b1
Luis Gabriel Ramos Robles <luisgabrielramosrobles@gmail.com>
2022-01-25 22:45:39 -0400

    Final Commit

--- src/components/LandingView/LandingViewComponents.tsx
+++ src/components/LandingView/LandingViewComponents.tsx
@@ -1,4 +1,3 @@
-import { ReactNode, VoidFunctionComponent } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { MainButtonCSS } from "../AbstractedComponents";
